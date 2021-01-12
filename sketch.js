9836f215152f96723e3702dd637c594cf3b3d42f
nadommer <74824558+nadommer@users.noreply.github.com>
2021-01-12 22:10:14 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,39 +1,82 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
-var engine, world;
-var car,wall
-var speed, weight
-function setup(){
-    var canvas = createCanvas(1600,400);
-    engine = Engine.create();
-    world = engine.world;
-    speed = random(55,90)
- weight = random(400,1500)
- car = createSprite(50,200,50,50);
- wall = createSprite(1500,200,60,height/2)
- car.velocityX = speed;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
 
-function draw(){
-    background(0);
-    Engine.update(engine);
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
-if (wall.x - car.x < (car.width=wall.width)/2)
-{
-car.velocityX = 0;
-var deformation = 0.5* wieght*speed*speed/22509;
-if (deformation > 100)
-(
-    car.shapeColor = color(255,0,0)
-)
-if (deformation < 100 && deformation>100)
-{
-    car.shapeColor = color(230,230,0)
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+ 	boxPosition=width/2-100
+ 	boxY=610;
+
+
+ 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
+
+
+	Engine.run(engine);
+  
 }
-if (deformation<100)
-{
-    car.shapeColor = color(0,255,0);
-}
-}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+ 
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+
+  Matter.Body.setStatic(packageBody,false)
+  drawSprites();
+  
+  
+ 
 }
